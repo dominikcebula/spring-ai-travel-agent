e1c719f20938e85346abb7770c564a44950a86e0
Dominik Cebula <dominikcebula@gmail.com>
2026-01-13 21:26:45 +0100

    spring-ai-travel-agent

---
+++ flights-mcp-server/src/main/java/com/dominikcebula/spring/ai/flights/FlightsMcpServerApplication.java
@@ -0,0 +1,12 @@
+package com.dominikcebula.spring.ai.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsMcpServerApplication.class, args);
+    }
+}
